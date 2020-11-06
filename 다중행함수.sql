0a300b5c72ce79f62334f98dedcb7faabe455a28
[Jun Heo] <[c_atuniel@naver.com]>
2020-11-06 17:12:16 +0900

    20201106 PM Save 1

--- 다중행함수.sql
+++ 다중행함수.sql
@@ -81,3 +81,135 @@ ORDER BY deptno, job ASC;
 
 -------------------------------------------------------------------------------------------
 
+-- 조인
+SELECT *
+FROM emp, dept
+ORDER BY empno;
+
+SELECT * 
+FROM dept; -- 4행
+
+SELECT * 
+FROM emp; -- 14행
+
+-- 나올 수 있는 모든 조합 조회
+SELECT COUNT(*)
+FROM emp, dept -- 총 행 수
+ORDER BY empno;
+
+-------------------------------------------------------------------------------------------
+-- 1. 내부조인(등가조인)
+SELECT *
+FROM emp, dept
+WHERE emp.deptno = dept.deptno -- 조인의 기준 ex) emp의 deptno와 dept의 deptno가 같으면 호출
+ORDER BY empno;
+
+SELECT *
+FROM emp INNER JOIN dept
+ON emp.deptno = dept.deptno  -- 조인의 기준
+ORDER BY empno;
+
+SELECT *
+FROM emp E, dept D -- FROM 테이블에 별칭부여 가능
+WHERE E.deptno = D.deptno -- 별칭으로 조인기준 반영 가능
+ORDER BY empno;
+
+SELECT *
+FROM emp E, dept D
+WHERE E.deptno = D.deptno -- 조인의 기준 ex) emp의 deptno와 dept의 deptno가 같으면 호출
+ORDER BY empno;
+
+-- 두 테이블에 같은 이름의 필드가 존재하는 경우
+-- 어느 테이블에 있는 필드를 호출할지 정확히 명시해야
+SELECT empno, ename, job, D.deptno, dname
+FROM emp E, dept D
+WHERE E.deptno = D.deptno -- 조인의 기준 ex) emp의 deptno와 dept의 deptno가 같으면 호출
+ORDER BY empno;
+
+-- EMP 테이블과 DEPT 테이블을 조인하여 empno, ename, sal, deptno, dname, loc조회(급여 3000이상만 정렬)
+SELECT E.empno, e.ename, e.sal, D.deptno, D.dname, D.loc
+FROM emp E, dept D
+WHERE E.deptno = D.deptno AND SAL >= 3000;
+
+-- emp 테이블의 별칭을 e로 dept 별칭 d로, 급여 2500 이하, 사원번호 9999이하 사원정보 출력
+SELECT *
+FROM emp E INNER JOIN dept D
+ON E.deptno = D.deptno AND sal <=2500 AND E.deptno <= 9999
+ORDER BY empno;
+
+-- emp 테이블의 별칭을 e로 salgrade 별칭 s로, 각 사원 정보랑 등급 출력
+SELECT *
+FROM emp E, salgrade S
+WHERE E.sal BETWEEN S.losal AND S.hisal;
+
+-------------------------------------------------------------------------------------------
+
+-- 2. 셀프조인(자체조인) : 조인테이블이 자기 자신 테이블일때
+SELECT * FROM emp;
+
+-- ↓ 출력 시 President 와 같은 NULL은 출력되지 않음 -> 이 경우 외부조인으로 추출 할 것
+SELECT E1.empno, E1.ename, E1.mgr, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1, emp E2
+WHERE E1.mgr = E2.empno;
+
+-------------------------------------------------------------------------------------------
+
+-- 3. 아우터조인(외부조인) : 조건을 만족하지 않는 데이터 추출
+SELECT * FROM emp;
+
+-- 3-1 LEFT OUTER JOIN
+-- 방법 1 ## 표준
+SELECT E1.empno, E1.ename, E1.mgr, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1 LEFT OUTER JOIN emp E2
+ON E1.mgr = E2.empno;
+
+-- 방법2 ## 구형
+SELECT E1.empno, E1.ename, E1.mgr, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1, emp E2
+WHERE E1.mgr = E2.empno(+);
+
+-- 3-2 RIGHT OUTER JOIN
+-- 방법 1 ## 표준
+SELECT E1.empno, E1.ename, E1.mgr, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1 RIGHT OUTER JOIN emp E2
+ON E1.mgr = E2.empno;
+
+-- 방법2 ## 구형
+SELECT E1.empno, E1.ename, E1.mgr, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1, emp E2
+WHERE E1.mgr(+) = E2.empno;
+
+-------------------------------------------------------------------------------------------
+
+-- 실습1
+SELECT E.deptno, D.dname, E.empno, E.Ename, sal 
+FROM emp E INNER JOIN dept D
+ON E.deptno = D.deptno AND sal > 2000
+ORDER BY E.deptno;
+
+-- 실습2
+SELECT E.deptno, D.dname, FLOOR(AVG(E.sal)) AS AVG_SAL, MAX(E.sal) AS MAX_SAL, MIN(E.sal) AS MIN_SAL, COUNT(E.empno) AS CNT
+FROM emp E INNER JOIN dept D
+ON E.deptno =  D.deptno
+group by E.deptno, D.dname;
+
+-- 실습3
+SELECT E.deptno, D.dname, E.empno, E.ename, E.job, E.sal
+FROM emp E RIGHT OUTER JOIN dept D
+ON E.deptno = D.deptno
+ORDER BY E.deptno, E.empno ASC;
+
+-- 실습4
+-- 방법1
+SELECT E1.deptno, D1.dname, E1.empno, E1.ename, E1.mgr, E1.sal, E1.deptno, S.losal, S.hisal, S.grade, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1
+RIGHT OUTER JOIN dept D1 ON E1.deptno = D1.deptno
+LEFT OUTER JOIN salgrade S ON E1.sal BETWEEN S.losal AND S.hisal
+LEFT OUTER JOIN emp E2 ON E1.mgr = E2.empno
+ORDER BY D1.deptno, E1.empno;
+
+--방법2
+SELECT E1.deptno, D1.dname, E1.empno, E1.ename, E1.mgr, E1.sal, E1.deptno, S.losal, S.hisal, S.grade, E2.empno AS MGR_EMPNO, E2.ename AS MGR_ENAME
+FROM emp E1, dept D1, salgrade S, emp E2
+WHERE E1.deptno(+) = D1.deptno AND E1.sal BETWEEN S.losal(+) AND S.hisal(+) AND E1.mgr = E2.empno(+)
+ORDER BY D1.deptno;
